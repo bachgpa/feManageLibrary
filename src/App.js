3e78c3b4d0eb37cd89c6d1f43f94a50f76dfe999
Bách Nguyễn <105241006+bachgpa@users.noreply.github.com>
2024-03-01 08:00:13 +0700

    making gg login

--- src/App.js
+++ src/App.js
@@ -15,46 +15,49 @@ import PrimaryPage from "./Pages/PrimaryPage";
 import LoginPage from "./Pages/LoginPage";
 import CategoriesPage from "./Pages/CategoriesPage";
 import UserPage from "./Pages/UserPage.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export const webThemeContext = createContext();
 
 function App() {
   const [theme, setTheme] = useState("light");
   return (
-    <webThemeContext.Provider value={{ theme, setTheme }}>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <DefaultLayout
-                pageContent={<PrimaryPage />}
-              />
-            }
-          />
-          <Route
-            path="/categories"
-            element={
-              <DefaultLayout
-                pageContent={<CategoriesPage />}
-              />
-            }
-          />
-          <Route
-            path="/user/:id"
-            element={
-              <DefaultLayout pageContent={<UserPage />} />
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <LoginLayout pageContent={<LoginPage />} />
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </webThemeContext.Provider>
+    <GoogleOAuthProvider clientId="someID">
+      <webThemeContext.Provider value={{ theme, setTheme }}>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <DefaultLayout
+                  pageContent={<PrimaryPage />}
+                />
+              }
+            />
+            <Route
+              path="/categories"
+              element={
+                <DefaultLayout
+                  pageContent={<CategoriesPage />}
+                />
+              }
+            />
+            <Route
+              path="/user/:id"
+              element={
+                <DefaultLayout pageContent={<UserPage />} />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <LoginLayout pageContent={<LoginPage />} />
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </webThemeContext.Provider>
+    </GoogleOAuthProvider>
   );
 }
 
